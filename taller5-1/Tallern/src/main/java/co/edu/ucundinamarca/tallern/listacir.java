983b5d80f5b7e2d8afb7baec932732836be36ef9
brajhanda <72575929+brajhanda@users.noreply.github.com>
2021-10-15 15:11:56 -0500

    s

---
+++ taller5-1/Tallern/src/main/java/co/edu/ucundinamarca/tallern/listacir.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.tallern;
+
+/**
+ *
+ * @author ZARAT
+ */
+public class listacir {
+    nodos ultimo;
+    public listacir(){
+        ultimo=null;
+        
+    }
+    public nodos vacio(){
+        return ultimo=null;
+    }
+    public listacir insertar(int elemento){
+        nodos nuevo=new nodos (elemento);
+      if(ultimo!=null){
+          nuevo.siguiente=ultimo.siguiente;
+          ultimo.siguiente=nuevo;
+          
+      }
+      ultimo=nuevo;
+      return this;
+    }
+    public void mostrarLista(){
+        nodos auxiliar=ultimo.siguiente;
+        String cadena="";
+        do{
+            cadena=cadena + "["+auxiliar.dato+"]->";
+        }while (auiliar!=ultimo.siguiente);
+        
+       
+
